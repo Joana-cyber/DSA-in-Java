e8e86b570ec5dcb60fd72a35d48cdb34fbcafab8
Miss_Jo <joana.teye@ashesi.edu.gh>
2022-08-31 08:54:02 +0000

    implemented QuickFind algorithm

---
+++ week-1/QuickFind.java
@@ -0,0 +1,35 @@
+/**
+ * Entry point for this project.
+ *
+ * @author Joana Mamley Teye
+ * @Date 31/08/2022
+ */
+
+public class QuickFind{
+
+    private int myArray[];
+
+
+
+    public QuickFind(int n){
+        myArray = new int[n];
+
+        for (int i = 0; i < n; i++){
+            myArray[i] = i;
+        }
+    }
+
+
+    public boolean connected(int firstNumber, int secondNumber){
+        return myArray[firstNumber] == myArray[secondNumber];
+    }
+
+    public void union(int firstNumber, int secondNumber){
+        int firstNumberEntry = myArray[firstNumber];
+        int secondNumberEntry = myArray[secondNumber];
+
+        for(int i=0; i<myArray.length; i++){
+            if(myArray[i] == firstNumberEntry) myArray[i] = secondNumberEntry;
+        }
+    }
+}
